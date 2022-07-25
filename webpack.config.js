f2e19894d64fa4b8205017876ca1c4d04867d1f2
MoSangBin <mosangbin94@gmail.com>
2022-07-25 15:19:32 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./src/script.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "script.bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -21,4 +23,11 @@ module.exports = {
       template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
+  devServer: {
+    // 개발 서버가 dist 폴더를 제공할 수 있도록 설정
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 8080,
+  },
 };
