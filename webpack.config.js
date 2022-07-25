5d6ec1028baf5aa35ae170e483243f57ee8ab041
MoSangBin <mosangbin94@gmail.com>
2022-07-25 15:31:14 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "production",
   entry: "./src/script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "script.bundle.js",
     publicPath: "/",
   },
